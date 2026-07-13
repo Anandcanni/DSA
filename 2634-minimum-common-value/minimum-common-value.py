3a10e706b81457d238b8d616c5dcd0a13fb1548e
CANNIZARO__CODESS <ananddikshit2006@gmail.com>
2026-07-13 15:13:31 +0530

    Time: 16 ms (13.37%) | Memory: 45.8 MB (8.87%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -0,0 +1,12 @@
+class Solution:
+    #anand
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        common = set(nums1) & set(nums2)
+
+        if common:
+            return min(common)
+
+        return -1
+        
+
+       
